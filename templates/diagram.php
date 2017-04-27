f2c94370f8e65d825954e979594ea16e1b3683af
Atsuyaku <thomas.mediav@gmail.com>
2017-04-27 12:44:52 +0200

    add local files

---
+++ templates/diagram.php
@@ -0,0 +1,13 @@
+<svg id="fillgauge1" width="97%" height="250" onclick="gauge1.update(NewValue());"></svg>
+<script language="JavaScript">
+    var config1 = liquidFillGaugeDefaultSettings();
+    var gauge1= loadLiquidFillGauge("fillgauge1", 50, config1);
+
+    function NewValue(){
+        if(Math.random() > .5){
+            return Math.round(Math.random()*100);
+        } else {
+            return (Math.random()*100).toFixed(1);
+        }
+    }
+</script>
---
+++ templates/head.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8" />
+<link rel="stylesheet" href="./src/css/style.css" />
+<link rel="stylesheet" href="./src/css/menu.css" />
+
+<script src="./src/js/jquery-3.1.1.min.js"></script>
+<script src="./src/js/jquery.nicescroll.js"></script>
+
+
+<script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
+<script src="./src/js/liquidFillGauge.js" language="JavaScript"></script>
+
+<script>
+	$(document).ready(function() {$("html").niceScroll({scrollspeed: 150});});
+</script>
