33f42b4f3efaf5b1e0b8821cd272d1a594afa471
Yunnx <Yunnx@users.noreply.github.com>
2017-04-27 18:44:42 +0200

    Update hist.php

--- hist.php
+++ hist.php
@@ -2,29 +2,19 @@
 <html>
 	<head>
 		<title>Projet SIN</title>
+		<?php require_once('./templates/head.php');?>
 	</head>
-<?php require_once('./templates/head.php');?>
 	<body>
 		<header>
 			<h1>Historique</h1>
 		</header>
-    <nav>
-    			<div id="menu">
-    				<ul>
-    					<li><a href="./index.php">Accueil</a></li>
-    					<li><a href="./hist.php">Historique</a></li>
-    					<li><a href="./stat.php">Statistiques</a></li>
-    					<li><a href="./config.php">Configuration</a></li>
-    				</ul>
-    			</div>
-    		</nav>
-		<nav><?php require_once('templates/nav.php');?></nav>
+		<nav>
+			<?php require_once('templates/nav.php');?>
+		</nav>
 		<div class="bordure">
 			<section>
-
 			</section>
-			</div>
-
+		</div>
 		<footer>
 			<h5>Site développé en 2016/2017 par des élèves de Terminale STI2D SIN: Mediavilla, Beaubestre, Bonnet, Magueur, Baqué </h5>
 		</footer>
