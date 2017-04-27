a167a1331452131df50edf575607e5bad82f6624
Yunnx <Yunnx@users.noreply.github.com>
2017-04-27 18:43:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 <html lang=fr>
 	<head>
 		<title>Projet SIN</title>
-			<meta charset="utf-8" />
+		<?php require_once('./templates/head.php');?>
 	</head>
-				<?php require_once('./templates/head.php');?>
 	<body>
 		<header>
 			<h1>Smart House</h1>
@@ -15,40 +14,24 @@
 			</section>
 		</div>
 		<nav>
-
-			<div id="menu">
-				<ul>
-					<li><a href="./index.php">Accueil</a></li>
-					<li><a href="./hist.php">Historique</a></li>
-					<li><a href="./stat.php">Statistiques</a></li>
-					<li><a href="./config.php">Configuration</a></li>
-				</ul>
-			</div>
-		</nav>
-		<nav>
 			<?php require_once('./templates/nav.php');?>
 		</nav>
 		<div id="text">
-				<ul>
-					<li>Contôlez votre maison depuis n'importe où en quelques clics</li>
-					<li>Soyez au courant de la température intérieure et de la luminosité extérieure</li>
-					<li>Obtenez un historique de ces données dans le temps</li>
-				</ul>
+			<ul>
+				<li>Contôlez votre maison depuis n'importe où en quelques clics</li>
+				<li>Soyez au courant de la température intérieure et de la luminosité extérieure</li>
+				<li>Obtenez un historique de ces données dans le temps</li>
+			</ul>
 		</div>
 		<div class="inscription">
-	<p>
-	 <a href="...">Inscription</a>
- </p>
-</div>
+			<p><a href="...">Inscription</a></p>
+		</div>
 		<div class="connexion">
-  <p>
-   <a href="...">Connection</a>
- </p>
-</div>
+			<p><a href="...">Connection</a></p>
+		</div>
 		<footer>
 			<h5>Site développé en 2016/2017 par des élèves de Terminale STI2D SIN: Mediavilla, Beaubestre, Bonnet, Magueur, Baqué </h5>
 		</footer>
-
 	</body>
 </html>
 
